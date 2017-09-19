d4f184713f9ab7ba6277acde8bfca5e2e9c01562
manbrk <manbrk@gmail.com>
2017-09-19 18:38:34 +0300

    add and save button fixes

--- src/app/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  @Input() editMode: string;
 
   constructor() { }
 
